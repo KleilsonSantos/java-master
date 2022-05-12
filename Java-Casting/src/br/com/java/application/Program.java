e8cc7063b6543f87e3e1631b5408ffa51c16bd24
KleilsonSantos <kdsdesign1@gmail.com>
2022-05-12 11:19:16 -0300

    Criando Java-Casting

---
+++ Java-Casting/src/br/com/java/application/Program.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package br.com.java.application;
+
+/**
+ * @author Kleilson Santos 
+ *
+ */
+
+import java.util.Locale;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		/* Declarando variáveis do tipo 'int' */
+		int age = 30;
+		int code = 5290;
+		
+		/* Declarando variável do tipo 'double' */
+		double resultDouble = age + code;
+		System.out.println(resultDouble);
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		/* Declarando variável do tipo 'int' e aplicando o casting*/
+		int resultInt = (int) (price1 + price2 + measure); 
+		System.out.println(resultInt);
+		
+	}
+
+}
