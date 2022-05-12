9e2893241fb9ac6621b841a0fa7c3c7405770d62
KleilsonSantos <kdsdesign1@gmail.com>
2022-05-12 11:32:14 -0300

    Atualizando Java-OperadoresMatematicos

--- Java-OperadoresMatematicos/src/br/com/java/application/Program.java
+++ Java-OperadoresMatematicos/src/br/com/java/application/Program.java
@@ -23,8 +23,28 @@ public class Program {
 		 * das variáveis 'valorA' + 'valorB'.*/
 		int resultadoSoma = valorA + valorB;
 		
+		/* Declarando variável para receber o valor da soma 
+		 * das variáveis 'valorA' + 'valorB'.*/
+		int resultadoSubtracao = valorA - valorB;
+		
+		/* Declarando variável para receber o valor da soma 
+		 * das variáveis 'valorA' + 'valorB'.*/
+		int resultadoMultiplicacao = valorA * valorB;
+		
+		/* Declarando variável para receber o valor da soma 
+		 * das variáveis 'valorA' + 'valorB'.*/
+		int resultadoDivisao = valorA / valorB;
+		
+		/* Declarando variável para receber o valor da soma 
+		 * das variáveis 'valorA' + 'valorB'.*/
+		int resultadoModulo = valorA % valorB;
+		
 		/* Imprimindo o valor da variável 'resultadoSoma'.*/
-		System.out.println(resultadoSoma);
+		System.out.println("Resuldate da soma de " + valorA + " + " + valorB + " -> " + resultadoSoma);
+		System.out.println("Resuldate da subtração de " + valorA + " - " + valorB + " -> " + resultadoSubtracao);
+		System.out.println("Resuldate da Multitplicação de " + valorA + " * " + valorB + " -> " + resultadoMultiplicacao);
+		System.out.println("Resuldate da divisão de " + valorA + " / " + valorB + " -> " + resultadoDivisao);
+		System.out.println("Resuldate do módulo de " + valorA + " % " + valorB + " -> " + resultadoModulo);
 		
 	}
 
