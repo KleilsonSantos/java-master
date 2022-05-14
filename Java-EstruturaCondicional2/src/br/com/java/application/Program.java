84417e1b6e1b1ae2c2a31518047ecdef6ae83b63
KleilsonSantos <kdsdesign1@gmail.com>
2022-05-14 19:19:40 -0300

    update Java-EstruturaCondicional2

--- Java-EstruturaCondicional2/src/br/com/java/application/Program.java
+++ Java-EstruturaCondicional2/src/br/com/java/application/Program.java
@@ -1,9 +1,7 @@
 /*
- *Uma operadora de telefonia cobra R$ 30.00 por um plano básico que
- *dá direito a 100 minutos de telefone. Cada minuto que exceder a
- *franquia de 100 minutos custa R$ 2.00. Fazer um programa para ler a
- *quantidade de minutos que uma pessoa consumiu, daí mostrar o valor
- *a ser pago.
+ * O programa lê um valor inteiro de 1 a 7 representando um
+ * dia da semana (sendo 1=domingo, 2=segunda, e assim por diante).
+ * Escrever na tela o dia da semana correspondente.
  * 
  */
 package br.com.java.application;
@@ -52,6 +50,7 @@ public class Program {
 			break;
 		}
 
+		// fechando o scanner.
 		scanner.close();
 	}
 }
