04f20ecbf1b012912ece250d384f2a3241206a9f
KleilsonSantos <kdsdesign1@gmail.com>
2022-05-16 19:34:04 -0300

    update Java-EstruturaRepetitiva

--- Java-EstruturaRepetitiva/src/Program.java
+++ Java-EstruturaRepetitiva/src/Program.java
@@ -18,8 +18,7 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
-		int value;
-		int result = 0;
+		int value,result = 0;
 		
 		try {
 			do {
