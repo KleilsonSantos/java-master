2091c99d67c8689a3e07c4639126c00ba0b9b8dc
KleilsonSantos <kdsdesign1@gmail.com>
2022-05-17 11:14:52 -0300

    creating folder

---
+++ myproject/src/main/java/br/com/springboot/myproject/MyFirstProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectSpringBootApplication.class, args);
+	}
+
+}
