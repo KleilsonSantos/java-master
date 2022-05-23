3e063a9dcf8d59174ad8acac651e7790ff7448b4
KleilsonSantos <kdsdesign1@gmail.com>
2022-05-23 20:34:04 -0300

    :up: Java-Interface

--- Java-Interface/src/br/com/java/section18/model/application/Program.java
+++ Java-Interface/src/br/com/java/section18/model/application/Program.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 import br.com.java.section18.model.entities.CarRental;
 import br.com.java.section18.model.entities.Vehicle;
@@ -15,27 +16,34 @@ public class Program {
 	public static void main(String[] args) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		Locale.setDefault(Locale.UK);
+		Scanner scanner = new Scanner(System.in);
+
 		try {
-			String model = "Civic";
-			Date in = sdf.parse("25/06/2018 10:30");
-			Date out = sdf.parse("25/06/2018 14:40");
-			double valuePerHour = 10.0;
-			double valuePerDay = 130.0;
-			
-			CarRental cR = new CarRental(in, out, new Vehicle(model));
-			RentalService rS = new RentalService(valuePerHour, valuePerDay, new BrazilTaxService());
-			rS.processingInvoice(cR);
-			
+			System.out.print("Model car:");
+			String model = scanner.next();
+			System.out.print("Date In: ");
+			Date in = sdf.parse(scanner.next());
+			System.out.print("Date Out: ");
+			Date out = sdf.parse(scanner.next());
+			System.out.print("Value per hour: ");
+			double valuePerHour = 10.00;
+			System.out.print("Value per day: ");
+			double valuePerDay = 130.00;
+
+			CarRental carRental = new CarRental(in, out, new Vehicle(model));
+			RentalService rentalService = new RentalService(valuePerHour, valuePerDay, new BrazilTaxService());
+			rentalService.processingInvoice(carRental);
+
 			System.out.println("INVOICE");
-			System.out.println("Basic payment: " + String.format("%.2f", cR.getInvoice().getBasicPayment()));
-			System.out.println("Tax: " + cR.getInvoice().getTax());
-			System.out.println("Total payment: " + String.format("%.2f", cR.getInvoice().getTotalPayment()));
+			System.out.println("Basic payment: " + String.format("%.2f", carRental.getInvoice().getBasicPayment()));
+			System.out.println("Tax: " + carRental.getInvoice().getTax());
+			System.out.println("Total payment: " + String.format("%.2f", carRental.getInvoice().getTotalPayment()));
 		} catch (ParseException e) {
 			System.err.println("Error: " + e.getMessage());
 		} catch (NullPointerException e) {
 			System.err.println("Error: " + e.getMessage());
 		}
-
+		scanner.close();
 	}
-	
+
 }
