8fa84cdeb285641e96e42bc64deaccea41c816c7
KleilsonSantos <kdsdesign1@gmail.com>
2022-05-31 00:25:31 -0300

    :package: Java-Generics

---
+++ Java-Generics/src/br/com/java/model/entities/Pessoa.java
@@ -0,0 +1,58 @@
+package br.com.java.model.entities;
+
+import java.util.Objects;
+
+public class Pessoa{
+
+	private String nome;
+	private String email;
+
+	public Pessoa() {
+	}
+
+	public Pessoa(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome, email);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return  Objects.equals(nome, other.nome) && Objects.equals(email, other.getEmail());
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + " - Email: " + email;
+	}
+
+
+}
